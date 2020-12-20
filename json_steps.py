53800b095357415debdf2b79bd19b0eff2963795
sam <sam@samnache.dev>
2020-12-20 14:05:30 -0800

    more steps

--- json_steps.py
+++ json_steps.py
@@ -8,3 +8,11 @@ with open('show_ip_ospf.json') as file:
 json_dict = json.loads(blob)
 
 pp(json_dict)
+
+json_dict['TABLE_ctx']['ROW_ctx']['TABLE_area']['ROW_area']['age'] = 'NO_AGE'
+
+json_dump = json.dumps(json_dict)
+
+json_dict = json.loads(json_dump)
+
+pp(json_dict)
