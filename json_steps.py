8c13acbc5bfc14df6ba2fb7988402e1aaa182b3e
sam <sam@samnache.dev>
2020-12-20 12:32:25 -0800

    json steps

--- json_steps.py
+++ json_steps.py
@@ -1,1 +1,10 @@
 import json
+from pprint import pprint as pp
+
+with open('show_ip_ospf.json') as file:
+    # read in as tuple of strings
+    blob = file.read()
+
+json_dict = json.loads(blob)
+
+pp(json_dict)
